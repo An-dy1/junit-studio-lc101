2353e899e49eea84f5e2f5e2e4b60f44a3b4b78d
Andie Johnson <ajohnson2@naic.org>
2022-04-06 11:44:07 -0500

    my solution so far

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -22,14 +22,36 @@ public class BalancedBrackets {
      * @return true if balanced, false otherwise
      */
     public static boolean hasBalancedBrackets(String str) {
-        int brackets = 0;
+        int left_brackets = 0;
+        int right_brackets = 0;
+
+        // figure out if two left brackets are following each other
+        // hashMap of left and right bracket pairs?
+
         for (char ch : str.toCharArray()) {
             if (ch == '[') {
-                brackets++;
-            } else if (ch == ']') {
-                brackets--;
+                left_brackets++;
+            } else if (ch == ']' && right_brackets == left_brackets - 1) {
+                right_brackets++;
             }
         }
-        return brackets == 0;
+        return left_brackets == right_brackets && left_brackets > 0;
     }
+
+//    public static boolean hasBalancedBrackets(String str) {
+//        int brackets = 0;
+//        for (char ch : str.toCharArray()) {
+//            if (brackets >= 0) {
+//                if (ch == '[') {
+//                    brackets ++;
+//                } else if (ch == ']') {
+//                    brackets --;
+//                }
+//            } else {
+//                return false;
+//            }
+//        }
+//        return brackets == 0;
+//    }
+
 }
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,6 +12,67 @@ public class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void onlyLeftBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch["));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrueWithMixedCharacters() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("My [Bracket] "));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrueWithSeveralMatches() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("My [Bracket] is [better] than [yours]"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsFalseParentheses() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("()"));
+    }
+
+    @Test
+    public void emptyStringReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void repeatedBracketWhenMatchedReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void equalBracketsInWrongOrderReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("] ["));
+    }
+
+    @Test
+    public void onlyRightBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("A st]ring"));
+    }
+
+
+//    @Test(expected = IllegalArgumentException.class)
+//    public void balancedBracketsDoesNotAcceptInt() {
+//        BalancedBrackets.hasBalancedBrackets();
+//    }
+
+    // onlyBrackets throws exception if passed an integer
+    // onlyBrackets throws exception if passed a double
+    // throws exception if passed an array of strings
+    // accepts string parameters (does not throw illegal argument exception)
+    // a string with un-matched brackets returns false
+    // a string with matched brackets and other characters returns true
+    // returns true if string passed has several sets of matched brackets
+    // returns false for matched curly brackets
+    // returns false for matched parentheses
 
 
 }

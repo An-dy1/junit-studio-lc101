68aaf7af65be7ce40ae4d521f24aede376c794ca
Andie Johnson <ajohnson2@naic.org>
2022-04-12 09:18:58 -0500

    latest updates

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -26,30 +26,63 @@ public class BalancedBrackets {
         int right_brackets = 0;
 
         for (char ch : str.toCharArray()) {
+            // if there's a left bracket, count it
             if (ch == '[') {
                 left_brackets++;
-            } else if (ch == ']' && left_brackets - right_brackets > 0) {
-                right_brackets++;
+            // if there is a right bracket
+            } else if (ch == ']') {
+                // and there are more left brackets than right brackets currently counted, count it
+                if(left_brackets - right_brackets > 0) {
+                    right_brackets++;
+                // (if there are equal brackets or fewer left than right, this means a right bracket is out of order
+                } else {
+                    return false;
+                }
             }
         }
+        // left and right brackets are equal, and there is at least one of the left brackets
         return left_brackets == right_brackets && left_brackets > 0;
     }
 
 
+    // SOLUTION CODE
 //    public static boolean hasBalancedBrackets(String str) {
 //        int brackets = 0;
+//        int counter = 0;
 //        for (char ch : str.toCharArray()) {
+//            // if there have been 0 brackets, equal brackets, or at least one left bracket
 //            if (brackets >= 0) {
+//                // and there is a left bracket, count it
 //                if (ch == '[') {
-//                    brackets ++;
+//                    brackets++;
+//                    counter++;
+//                // or if there is a right bracket, count it (by removing from total count of brackets)
 //                } else if (ch == ']') {
 //                    brackets --;
 //                }
+//            // otherwise, there is a right bracket without a mate, so return false right away
 //            } else {
 //                return false;
 //            }
 //        }
-//        return brackets == 0;
+//        return brackets == 0 && counter > 0;
 //    }
 
+    // A SIMILAR DRAFT TO MINE
+//    public static boolean hasBalancedBrackets(String str) {
+//        int left_brackets = 0;
+//        int right_brackets = 0;
+//
+//        for (char ch : str.toCharArray()) {
+//            // if there's a left bracket, count it
+//            if (ch == '[') {
+//                left_brackets++;
+//                // if there is a right bracket, and there are more left brackets than right brackets currently counted, count it (if there are even counted brackets, this means that a right bracket is out of order)
+//            } else if (ch == ']' && left_brackets - right_brackets > 0) {
+//                right_brackets++;
+//            }
+//        }
+//        // left and right brackets are equal, and there is at least one of the left brackets
+//        return left_brackets == right_brackets && left_brackets > 0;
+//    }
 }
